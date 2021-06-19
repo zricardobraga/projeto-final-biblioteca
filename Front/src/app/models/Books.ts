1ef7cb208e3bc6324910ebdff3fad73cd9cb7539
ErikaDias <eka.rdds@hotmail.com>
2021-06-19 16:17:07 -0300

    Incluimos o Header, Barra de Navegação, Footer e inicio do List

---
+++ Front/src/app/models/Books.ts
@@ -0,0 +1,6 @@
+export class Books { 
+    _id?: string;
+    cod!: string;
+    title!: string;
+    status!: string;
+};
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,8 @@
 import express, {Request, Response} from "express";
+import cors from "cors";
 import { router } from "../config/routes";
 import { mongoose } from "../config/database";
+
 
 const app = express();
 
@@ -8,6 +10,7 @@ const db = mongoose;
 
 console.clear();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
